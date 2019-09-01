e81578ce5ca0b5f804beeb2d8913d60e289ade09
Yeongjun.Kim <yeongjun.kim@navercorp.com>
2019-09-01 19:03:42 +0900

    Add tutorial part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,13 @@ module.exports = {
   },
   /* Your site config here */
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
     "gatsby-plugin-sass",
     {
       resolve: "gatsby-plugin-typography",
