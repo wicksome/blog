cf5e43e899dad3a39b2d13718d2454d627dff231
Yeongjun.Kim <yeongjun.kim@navercorp.com>
2019-09-01 19:08:05 +0900

    Add tutorial part 6

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    `gatsby-transformer-remark`,
     "gatsby-plugin-sass",
     {
       resolve: "gatsby-plugin-typography",
